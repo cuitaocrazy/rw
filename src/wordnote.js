7b79b2bba60d974f250e116da0d734c55cfae926
cuitao <cuitaocrazy@163.com>
2018-05-02 13:27:31 +0800

    add word note

---
+++ src/wordnote.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import WordNote from './Pages/WordNote'
+
+const container = document.getElementById('container')
+
+render(<WordNote />, container)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
+
 module.exports = {
   entry: {
-    popup: './src/popup.js'
+    popup: './src/popup.js',
+    wordnote: './src/wordnote.js',
   },
   output: {
     path: path.resolve('./extensions'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
-    rules: [{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }]
-  }
+    rules: [{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }],
+  },
 }
