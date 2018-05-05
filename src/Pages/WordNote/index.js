48b9f278b256ff9d968f8bb1d9ad7c07a28f4baf
cuitaocrazy <tao.cui@bjyada.com>
2018-05-05 15:46:51 +0800

    完成编辑

--- src/Pages/WordNote/index.js
+++ src/Pages/WordNote/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import { observable } from 'mobx'
 import { addOrOupdate, del } from '../../words-api'
 import style from './wordnode.css'
 
@@ -7,14 +8,15 @@ const AddWord = props => {
   let wordInput
   let remarkInput
   return (
-    <form className={style.form}
+    <form
+      className={style.form}
       onSubmit={e => {
         addOrOupdate(wordInput.value, remarkInput.value)
         e.preventDefault()
       }}
     >
       <div className={style['input-en']}>
-        <input type="text" ref={el => (wordInput = el)} placeholder=" "/>
+        <input type="text" ref={el => (wordInput = el)} placeholder=" " />
         <span>English words</span>
       </div>
       <div className={style['input-ch']}>
@@ -25,16 +27,33 @@ const AddWord = props => {
     </form>
   )
 }
-const WordCard = props => (
-  <li className={style.card}>
-    <div className={style['card-word']}>{props.word}</div>
-    <button className={style['card-edit']}>
-      <img src="./images/edit.png" />
-    </button>
-    <div className={style['card-remark']}>{props.remark}</div>
-    <button className={style['card-del']} onClick={() => del(props.word)} />
-  </li>
-)
+const WordCard = observer(props => {
+  let inputRef
+  return (
+    <li className={style.card}>
+      <div className={style['card-word']}>{props.cardState.word}</div>
+      <button className={style['card-edit']} onClick={() => (props.cardState.isEdit = !props.cardState.isEdit)}>
+        <img src="./images/edit.png" />
+      </button>
+      {props.cardState.isEdit ? (
+        <div>
+          <form
+            onSubmit={evt => {
+              evt.preventDefault()
+              addOrOupdate(props.cardState.word, inputRef.value)
+            }}
+          >
+            <input defaultValue={props.cardState.remark} ref={ele => (inputRef = ele)} autoFocus />
+          </form>
+        </div>
+      ) : (
+        <div className={style['card-remark']}>{props.cardState.remark}</div>
+      )}
+
+      <button className={style['card-del']} onClick={() => del(props.word)} />
+    </li>
+  )
+})
 
 const Filter = props => (
   <div className={style['filter-content']}>
@@ -43,7 +62,8 @@ const Filter = props => (
   </div>
 )
 
-const createWordCards = words => Object.keys(words).map(word => <WordCard key={word} word={word} remark={words[word]} />)
+const createWordCards = words =>
+  Object.keys(words).map(word => <WordCard key={word} cardState={observable({ isEdit: false, word: word, remark: words[word] })} />)
 
 export default observer(props => (
   <div className={style['cards']}>
