155d24e81657f0de5cc0283d4c6916c677304ed9
cuitao <cuitaocrazy@163.com>
2018-05-09 09:55:59 +0800

    优化

---
+++ main.js
@@ -0,0 +1,3 @@
+const tk = require('./src/tk')
+
+console.log(tk('change\r', '423823.1359611753'))
--- src/background.js
+++ src/background.js
@@ -2,44 +2,37 @@ const tk = require('./tk')
 
 const regex = /TKK=eval\(\'(.*?)\'\)/ // eslint-disable-line
 
-/**
- * @return {Promise} p
- */
-function updateTkk() {
-  return fetch('https://translate.google.cn/')
-    .then(res => res.text())
-    .then(txt => regex.exec(txt)[1])
-    .then(js => eval(eval('"' + js + '"')))
-    .then(
-      tkk => new Promise((resolve, reject) => chrome.storage.local.set({ tkk: { key: tkk, timestamp: Math.floor(Date.now() / 3600000) } }, () => resolve(tkk)))
-    )
+const chget = keys => new Promise((resolve, reject) => chrome.storage.local.get(keys, resolve))
+const chset = obj => new Promise((resolve, reject) => chrome.storage.local.set(obj, resolve))
+
+async function saveTkk(tkk) {
+  await chset({ tkk: { key: tkk, timestamp: Math.floor(Date.now() / 3600000) } })
 }
 
-/**
- * @return {Promise} p
- */
-function checkTkk() {
-  return new Promise((resolve, reject) => {
-    const up = () =>
-      updateTkk()
-        .then(resolve)
-        .catch(reject)
-    chrome.storage.local.get(['tkk'], result => {
-      if (result.tkk) {
-        const now = Math.floor(Date.now() / 3600000)
-        if (result.tkk.timestamp != now) {
-          up()
-        } else {
-          resolve(result.tkk.key)
-        }
-      } else {
-        up()
-      }
-    })
-  })
+function atomPromiseFunc(fn) {
+  let p = Promise.resolve()
+  return function(...args) {
+    return (p = p.then(() => fn(...args)))
+  }
 }
 
-checkTkk()
+async function _getTkk() {
+  const tkk = await chget(['tkk'])
+  if (tkk && tkk.timestamp == Math.floor(Date.now() / 3600000)) {
+    return tkk.key
+  } else {
+    const res = await fetch('https://translate.google.cn/')
+    const txt = await res.text()
+    const js = regex.exec(txt)[1]
+    const key = eval(eval('"' + js + '"'))
+    saveTkk(key)
+    return key
+  }
+}
+
+const getTkk = atomPromiseFunc(_getTkk)
+
+getTkk()
 
 const makeUrl = (word, tk, dts) => 'https://translate.google.cn/translate_a/single?client=t&sl=en&tl=zh-CN&hl=zh-CN' + dts + tk + '&q=' + word
 
@@ -49,7 +42,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (cache[request.word]) {
       sendResponse({ data: cache[request.word] })
     } else {
-      checkTkk()
+      getTkk()
         .then(tkk => {
           const url = makeUrl(request.word, tk(request.word, tkk), request.dts)
           return fetch(url).then(res => res.json())
